d1149723030fc0252c93320bd685a7c4aaef7bf6
Fran2007 <franklinevillanueva4@gmail.com>
2023-12-11 23:50:26 -0600

    ➕ADD :add the form

---
+++ src/lib/SignupValidation.tsx
@@ -0,0 +1,14 @@
+import {z} from "zod"
+
+const SignupValidation = z.object({
+  name: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  email: z.string().email(),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters.",
+  }),
+})
+
+export default SignupValidation;
+ 
